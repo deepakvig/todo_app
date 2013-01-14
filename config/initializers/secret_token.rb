6ae526c7227edb0c2d42b081d31008bd0563e8a1
Deepak Vig <deepakvig@gmail.com>
2013-01-14 23:49:10 +0530

    ToDo app structure added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = 'd4a5839a48ab86b310bd719d869f999cea3a2044b14847532afdb7db7b7a0cbd31bad9671158c690e56aa5a8b509bc9edfc8510d42d1c6b9499cc6f8e5f56fd8'
