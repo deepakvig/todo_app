913c7c25c82822be35cfbafc9c08cc191d41075f
Deepak Vig <deepakvig@gmail.com>
2013-01-16 14:04:43 +0530

    Styling added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Task.create([{ name: 'A' }, { name: 'B' },{ name: 'C' }, { name: 'D' }])
+#Task.create([{ name: 'A' }, { name: 'B' },{ name: 'C' }, { name: 'D' }])
